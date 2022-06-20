cff79194a07cbae101fd12b61a9e78b8b377f92e
RunranLi <2271704437@qq.com>
2022-06-20 15:33:21 -0700

    step3

--- CoinCollection-Unity/Assets/Scripts/Collectable.cs
+++ CoinCollection-Unity/Assets/Scripts/Collectable.cs
@@ -31,6 +31,7 @@ public class Collectable : MonoBehaviour
 
         if (other.tag == "Player")
         {
+            other.GetComponent<Collection>().AddToCollection(); //call method on the Collection component of other object
             Destroy(gameObject); //destroy this gameObject (collectable object)
         }
     }//end OnTriggerEnter()
---
+++ CoinCollection-Unity/Assets/Scripts/Collection.cs
@@ -0,0 +1,56 @@
+﻿/****
+ * Created by: Runran Li
+ * Date Created: June 20, 2022
+ *
+ * Last Edited by:
+ * Last Edited:
+ *
+ * Description: Manages collections and wining conditions.
+****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    /***VARIABLES***/
+    [Tooltip("Number of collectables to beat level")]
+    public int winCollectAmount; //number of collectables to beat the level
+    [Tooltip("Use the total number of collectables for the win amount")]
+    public bool useCollectableCount; //Do we use the collectable countt
+    [HideInInspector]
+    private bool hasCollectedAll = false; //have all collectables been collected
+    private int collectablesInCollection = 0; //number of collectables collected by player 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if we are using the collectable count
+        if (useCollectableCount)
+        {
+            //set the win amount to the amout of collectables in the scene
+            winCollectAmount = Collectable.collectableCount;
+        }//end if(useCollectableCount)
+        Debug.Log("Win collect amount: " + winCollectAmount);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collectablesInCollection == winCollectAmount)
+        {
+            hasCollectedAll = true;
+            Debug.Log("You win!");
+        }
+
+    }
+
+    //Add to collection
+    public void AddToCollection()
+    {
+        collectablesInCollection++; //add to cmount in collection
+        Debug.Log("Collectable Added");
+    }//end AddToCollection()
+
+}
